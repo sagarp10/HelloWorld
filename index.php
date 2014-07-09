a5250799fca52198c450d26c17568494258a4d80
sagarp10 <sagarp1093@gmail.com>
2014-07-09 17:38:58 +0100

    Update index.php
    Added some code. if statement and for loop.

--- index.php
+++ index.php
@@ -1,5 +1,16 @@
 <?php
 
 echo 'hello world';
+var $bol = true;
 
+$collabNames = array("Sagar", "Keston");
+var $colabs = sizeof($collabNames)
+if($bol)
+{
+  echo "This Repository currently has " + $colabs+ " collaborators."
+  for(var $x = 0; $x<$colabs; $x++)
+  {
+    echo "\n Collaborator "+ $x + " :" + $collabNames[x];
+  }
+}
 ?>
